e5eb06d6f3caff15f3abd35c6c21135f93fa4eb7
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2021-06-01 22:57:47 +0200

    Remove underline highlighting.
    Those are confusing with some fonts. In the next commit, we should be
    able to override highlightings more correctly, especially via
    autocommands.
    
    Relates to #70.

--- lua/hop/highlight.lua
+++ lua/hop/highlight.lua
@@ -4,10 +4,10 @@ local M = {}
 -- Insert the highlights that Hop uses.
 function M.insert_highlights()
   -- Highlight used for the mono-sequence keys (i.e. sequence of 1).
-  vim.api.nvim_command('highlight default HopNextKey  guifg=#ff007c gui=bold,underline ctermfg=198 cterm=bold,underline')
+  vim.api.nvim_command('highlight default HopNextKey  guifg=#ff007c gui=bold ctermfg=198 cterm=bold')
 
   -- Highlight used for the first key in a sequence.
-  vim.api.nvim_command('highlight default HopNextKey1 guifg=#00dfff gui=bold,underline ctermfg=45 cterm=bold,underline')
+  vim.api.nvim_command('highlight default HopNextKey1 guifg=#00dfff gui=bold ctermfg=45 cterm=bold')
 
   -- Highlight used for the second and remaining keys in a sequence.
   vim.api.nvim_command('highlight default HopNextKey2 guifg=#2b8db3 ctermfg=33')
