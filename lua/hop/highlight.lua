5901f0e7928d8561800e3ca228c17618b686c532
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2022-10-09 12:57:24 +0200

    Link HopPreview to IncSearch by default.
    That will allow a more consistent style for some colorschemes.

--- lua/hop/highlight.lua
+++ lua/hop/highlight.lua
@@ -21,7 +21,7 @@ function M.insert_highlights()
   vim.api.nvim_command('highlight default link HopCursor Cursor')
 
   -- Highlight used for preview pattern
-  vim.api.nvim_command('highlight default HopPreview  guifg=#b8bb26 gui=bold ctermfg=142 cterm=bold')
+  vim.api.nvim_command('highlight link HopPreview IncSearch')
 end
 
 function M.create_autocmd()
